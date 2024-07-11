a3041e46e4784afc392f450944375b9812df6539
Caleb Chang <changcaleb2007@gmail.com>
2024-07-11 10:43:21 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import keras
+import numpy as np
 from joblib import load
 
 st.title("Dominant Emotion Classifier Demo")
@@ -8,7 +9,7 @@ st.header("By: Aditi, Angela, Caleb, Coco, Roshan")
 
 nn = keras.models.load_model("neuralnetwork.keras")
 
-def getUserInput():
+with st.form("input"):
     age = st.number_input("What is your age?", 0, 100, 0, 1)
     gender = st.selectbox("What is your gender?", ["Male", "Female", "Non-binary"])
     platform = st.selectbox("Which social media platform do you use?", ["Facebook", "Instagram", "LinkedIn", "Snapchat", "Telegram", "Twitter", "Whatsapp"])
@@ -17,8 +18,7 @@ def getUserInput():
     likes_received_per_day = st.number_input("How many likes do you receive per day?")
     comments_per_day = st.number_input("How many comments do you receive per day?")
     msg_per_day = st.number_input("How many messages do you send per day? (Includes sending vids/imgs)")
-    return [[age], [gender], [platform], [min_per_day], [posts_per_day], [likes_received_per_day], [comments_per_day], [msg_per_day]]
 
-x = getUserInput()
-
-st.write(nn.predict(x))
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write(nn.predict(np.array([[age], [gender], [platform], [min_per_day], [posts_per_day], [likes_received_per_day], [comments_per_day], [msg_per_day]])))
