9e5215358ba7244f6e25b188fc97fba42e745db0
Caleb Chang <changcaleb2007@gmail.com>
2024-07-11 10:23:57 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from joblib import load
+
+st.title("Dominant Emotion Classifier Demo")
+st.header("By: Aditi, Angela, Caleb, Coco, Roshan")
+
+nn = load("neuralnetwork.joblib")
+
+def getUserInput():
+    age = st.number_input("What is your age?", 0, 100, 0, int)
+    gender = st.selectbox("What is your gender?", ["Male", "Female", "Non-binary"])
+    platform = st.selectbox("Which social media platform do you use?", ["Facebook", "Instagram", "LinkedIn", "Snapchat", "Telegram", "Twitter", "Whatsapp"])
+    min_per_day = st.number_input("How many minutes a day do you use this platform?")
+    posts_per_day = st.number_input("How many posts do you post per day?")
+    likes_received_per_day = st.number_input("How many likes do you receive per day?")
+    comments_per_day = st.number_input("How many comments do you receive per day?")
+    msg_per_day = st.number_input("How many messages do you send per day? (Includes sending vids/imgs)")
+    return [[age], [gender], [platform], [min_per_day], [posts_per_day], [likes_received_per_day], [comments_per_day], [msg_per_day]]
+
+x = getUserInput()
+
+st.write(nn.predict(x))
