302071df0c91ceddbeb25d051a1a79daee9ced30
Caleb Chang <changcaleb2007@gmail.com>
2024-07-11 11:44:54 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ with st.form("input"):
         platform = 6
 
     if submitted:
-        x = np.array([[age], [gender], [platform], [min_per_day], [posts_per_day], [likes_received_per_day], [comments_per_day], [msg_per_day]])
+        x = np.array([[age, gender, platform, min_per_day, posts_per_day, likes_received_per_day, comments_per_day, msg_per_day]])
         pred = nn.predict(x)
         pred = np.argamx(pred, axis=1)
         st.write("Our model predicts that your dominant emotion is: ", emotion_labels[pred])
